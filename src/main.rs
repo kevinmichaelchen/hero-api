f404580fce6840b410896b86d4a2d2008565d240
Kevin Chen <kevinmichaelchen@gmail.com>
2018-12-13 15:46:34 -0500

    Actually insert data into the table (#2)
    * table macro not found in schema.rs; Diesel derive macros not found in scope; import from schema not working
    
    * file separation
    
    * fmt
    
    * bring in Diesel crate
    
    * do not double import Diesel
    
    * still have errors
    
    * :hear_no_evil: the table_name had to match schema.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(proc_macro_hygiene, decl_macro)]
+#![feature(uniform_paths)]
 
 #[macro_use]
 extern crate rocket;
@@ -10,33 +11,22 @@ extern crate rocket_contrib;
 extern crate serde_derive;
 
 use rocket::Rocket;
-use rocket_contrib::databases::diesel;
+
+#[macro_use]
+extern crate diesel;
+//use rocket_contrib::databases::diesel;
+
 use rocket_contrib::json::Json;
 
 #[database("my_db")]
 struct MyDatabase(diesel::MysqlConnection);
 
-//mod hero;
-//use hero::Hero;
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Hero {
-    pub id: Option<i32>,
-    pub name: String,
-    pub identity: String,
-    pub hometown: String,
-    pub age: i32,
-}
+mod hero;
+mod schema;
+use hero::Hero;
 
-impl Hero {
-    pub fn create(conn: &diesel::MysqlConnection, hero: Json<Hero>) -> Json<Hero> {
-        hero
-    }
-}
-
-
-
-
-
+#[cfg(test)]
+mod tests;
 
 #[get("/")]
 fn hello() -> &'static str {
@@ -45,7 +35,11 @@ fn hello() -> &'static str {
 
 #[post("/", data = "<hero>")]
 fn create(conn: MyDatabase, hero: Json<Hero>) -> Json<Hero> {
-    Hero::create(&conn.0, hero)
+    let insert = Hero {
+        id: None,
+        ..hero.into_inner()
+    };
+    Json(Hero::create(&conn.0, &insert))
 }
 
 fn rocket() -> Rocket {
@@ -57,44 +51,4 @@ fn rocket() -> Rocket {
 
 fn main() {
     rocket().launch();
-}
-
-#[cfg(test)]
-mod tests {
-    use super::rocket;
-    use rocket::http::Status;
-    use rocket::local::Client;
-    use crate::Hero;
-    //    use crate::hero::Hero;
-
-    #[test]
-    fn test_hello() {
-        let client = Client::new(rocket()).unwrap();
-        let mut response = client.get("/").dispatch();
-        assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.body_string(), Some("Hello, world!".into()));
-    }
-
-    #[test]
-    fn it_works() {
-        let hero = Hero {
-            id: Some(1),
-            name: String::from("Superman"),
-            identity: String::from("Clark Kent"),
-            hometown: String::from("Metropolis"),
-            age: 32,
-        };
-        let serialized = serde_json::to_string(&hero).unwrap();
-        println!("serialized = {}", serialized);
-        assert_eq!(serialized, r#"{"id":1,"name":"Superman","identity":"Clark Kent","hometown":"Metropolis","age":32}"#);
-
-        let deserialized: Hero = serde_json::from_str(&serialized).unwrap();
-        println!("deserialized = {:?}", deserialized);
-
-        assert_eq!(deserialized.id, Some(1));
-        assert_eq!(deserialized.name, "Superman");
-        assert_eq!(deserialized.identity, "Clark Kent");
-        assert_eq!(deserialized.hometown, "Metropolis");
-        assert_eq!(deserialized.age, 32);
-    }
 }
---
+++ src/tests.rs
@@ -0,0 +1,38 @@
+use super::hero::Hero;
+use super::rocket;
+use rocket::http::Status;
+use rocket::local::Client;
+
+#[test]
+fn test_hello() {
+    let client = Client::new(rocket()).unwrap();
+    let mut response = client.get("/").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some("Hello, world!".into()));
+}
+
+#[test]
+fn it_works() {
+    let hero = Hero {
+        id: Some(1),
+        name: String::from("Superman"),
+        identity: String::from("Clark Kent"),
+        hometown: String::from("Metropolis"),
+        age: 32,
+    };
+    let serialized = serde_json::to_string(&hero).unwrap();
+    println!("serialized = {}", serialized);
+    assert_eq!(
+        serialized,
+        r#"{"id":1,"name":"Superman","identity":"Clark Kent","hometown":"Metropolis","age":32}"#
+    );
+
+    let deserialized: Hero = serde_json::from_str(&serialized).unwrap();
+    println!("deserialized = {:?}", deserialized);
+
+    assert_eq!(deserialized.id, Some(1));
+    assert_eq!(deserialized.name, "Superman");
+    assert_eq!(deserialized.identity, "Clark Kent");
+    assert_eq!(deserialized.hometown, "Metropolis");
+    assert_eq!(deserialized.age, 32);
+}
