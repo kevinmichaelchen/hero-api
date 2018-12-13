3e20d7ead632a8a23341aa264e01c12837f2b0f4
Kevin Chen <kevinmichaelchen@gmail.com>
2018-12-13 16:30:54 -0500

    Other crud endpoints (#3)
    * PUT, DELETE, and GET work
    
    * Remove random hard-coded mock

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,14 @@ use rocket::Rocket;
 extern crate diesel;
 //use rocket_contrib::databases::diesel;
 
-use rocket_contrib::json::Json;
+use rocket_contrib::json::{Json, JsonValue};
 
 #[database("my_db")]
 struct MyDatabase(diesel::MysqlConnection);
 
 mod hero;
 mod schema;
-use hero::Hero;
+use hero::{Hero, HeroWithId};
 
 #[cfg(test)]
 mod tests;
@@ -34,17 +34,40 @@ fn hello() -> &'static str {
 }
 
 #[post("/", data = "<hero>")]
-fn create(conn: MyDatabase, hero: Json<Hero>) -> Json<Hero> {
+fn create(conn: MyDatabase, hero: Json<Hero>) -> Json<HeroWithId> {
     let insert = Hero {
-        id: None,
         ..hero.into_inner()
     };
     Json(Hero::create(&conn.0, &insert))
 }
 
+#[get("/")]
+fn read(conn: MyDatabase) -> Json<JsonValue> {
+    Json(json!(Hero::read(&conn.0)))
+}
+
+#[put("/<id>", data = "<hero>")]
+fn update(conn: MyDatabase, id: i32, hero: Json<HeroWithId>) -> Json<JsonValue> {
+    // TODO should they be updating *with* ID?
+    let update = HeroWithId {
+        ..hero.into_inner()
+    };
+    Json(json!({
+        "success": Hero::update(&conn.0, id, update)
+    }))
+}
+
+#[delete("/<id>")]
+fn delete(conn: MyDatabase, id: i32) -> Json<JsonValue> {
+    Json(json!({
+        "success": Hero::delete(&conn.0, id)
+    }))
+}
+
 fn rocket() -> Rocket {
     rocket::ignite()
-        .mount("/hero", routes![create])
+        .mount("/hero", routes![create, update, delete])
+        .mount("/heroes", routes![read])
         .mount("/", routes![hello])
         .attach(MyDatabase::fairing())
 }
