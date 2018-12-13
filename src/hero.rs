469578215c628a0758a32ad962ac0c02245e428d
Kevin Chen <kevinmichaelchen@gmail.com>
2018-12-13 10:56:31 -0500

    Unit tests for serde

--- src/hero.rs
+++ src/hero.rs
@@ -1,4 +1,4 @@
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Hero {
     pub id: Option<i32>,
     pub name: String,
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#[macro_use]
+extern crate serde_derive;
+
 fn main() {
     println!("{}", "hello world")
+}
+
+mod hero;
+
+#[cfg(test)]
+mod tests {
+
+    use crate::hero::Hero;
+    #[test]
+    fn it_works() {
+        let hero = Hero {
+            id: Some(1),
+            name: String::from("Superman"),
+            identity: String::from("Clark Kent"),
+            hometown: String::from("Metropolis"),
+            age: 32,
+        };
+        let serialized = serde_json::to_string(&hero).unwrap();
+        println!("serialized = {}", serialized);
+        assert_eq!(serialized, r#"{"id":1,"name":"Superman","identity":"Clark Kent","hometown":"Metropolis","age":32}"#);
+
+        let deserialized: Hero = serde_json::from_str(&serialized).unwrap();
+        println!("deserialized = {:?}", deserialized);
+
+        assert_eq!(deserialized.id, Some(1));
+        assert_eq!(deserialized.name, "Superman");
+        assert_eq!(deserialized.identity, "Clark Kent");
+        assert_eq!(deserialized.hometown, "Metropolis");
+        assert_eq!(deserialized.age, 32);
+    }
 }
