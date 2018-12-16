747581e362f9f5800db5aaddbc9192e127ecb92a
Kevin Chen <kevinmichaelchen@gmail.com>
2018-12-15 20:32:15 -0500

    Add delete button (#22)

--- ui/src/Home/index.js
+++ ui/src/Home/index.js
@@ -6,8 +6,32 @@ import {
   Selectors as HeroesSelectors,
   ActionCreators as HeroesActionCreators,
 } from '../duck';
-import HeroList from '../HeroList';
 import { Link } from 'react-router-dom';
+
+class Hero extends React.PureComponent {
+  handleDelete = () => this.props.deleteHeroRequest(this.props.hero.id);
+
+  render() {
+    const { hero } = this.props;
+    return (
+      <div>
+        <Link to={`/hero/${hero.id}`}>
+          <pre>{JSON.stringify(hero)}</pre>
+        </Link>
+        <button onClick={this.handleDelete}>Delete</button>
+      </div>
+    );
+  }
+}
+
+const HeroList = ({ heroes, deleteHeroRequest }) => (
+  <div>
+    <h1>Superheroes</h1>
+    {heroes.map((h, i) => (
+      <Hero deleteHeroRequest={deleteHeroRequest} hero={h} key={i} />
+    ))}
+  </div>
+);
 
 class Home extends React.Component {
   componentDidMount() {
@@ -19,12 +43,17 @@ class Home extends React.Component {
 
   render() {
     const { heroes, loading, error } = this.props;
+    const {
+      actions: { deleteHeroRequest },
+    } = this.props;
     console.log('heroes =', heroes);
     return (
       <div>
         <Link to="/create">Create New Hero</Link>
         {/*<button onClick={() => console.log('click')}>Create New Hero</button>*/}
-        {heroes && <HeroList heroes={heroes} />}
+        {heroes && (
+          <HeroList deleteHeroRequest={deleteHeroRequest} heroes={heroes} />
+        )}
       </div>
     );
   }
@@ -40,6 +69,7 @@ const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators(
     {
       fetchHeroesRequest: HeroesActionCreators.fetchHeroesRequest,
+      deleteHeroRequest: HeroesActionCreators.deleteHeroRequest,
     },
     dispatch
   ),
